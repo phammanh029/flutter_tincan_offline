c7bc02c79266804b30ebf60ad46d20cfbff56f10
manhp <phammanh029@gmail.com>
2019-11-17 17:50:29 +0700

    - update api android

--- ios/Classes/TincanFlutterPlugin.m
+++ ios/Classes/TincanFlutterPlugin.m
@@ -1,8 +1,20 @@
 #import "TincanFlutterPlugin.h"
-#import <tincan_flutter/tincan_flutter-Swift.h>
 
 @implementation TincanFlutterPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftTincanFlutterPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"tincan_flutter"
+            binaryMessenger:[registrar messenger]];
+  TincanFlutterPlugin* instance = [[TincanFlutterPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
